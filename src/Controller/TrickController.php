fbde2178ffc19585d9a8c0861fa3b17094b3d376
francisL <fr.libs@gmail.com>
2020-04-26 09:42:18 +0200

    Modify TrickController to add the show function

---
+++ src/Controller/TrickController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trick;
+use App\Repository\TrickRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class TrickController extends AbstractController
+{
+    /**
+     * @Route("/tricks", name="tricks")
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * @Route("/tricks/{slug}-{id}", name="trick.show", requirements={"slug": "[a-z0-9\-]*"} )
+     * @param Trick $trick
+     * @return Response
+     */
+    public function show(Trick $trick, string $slug): Response
+    {
+        if($trick->getSlug() !== $slug)
+        {
+            return $this->redirectToRoute('trick.show', [
+                'id'    =>  $trick->getId(),
+                'slug'  =>  $trick->getSlug()
+            ],
+            301);
+        }
+       return $this->render('trick/show.html.twig', [
+           'trick'  =>  $trick,
+           'current-menu'   =>  'tricks'
+       ]);
+    }
+}
